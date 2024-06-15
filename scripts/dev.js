c634c76a6558599d750fdab7fd32f7ac92425502
Joseph <115842661+99JSP@users.noreply.github.com>
2024-06-15 10:58:25 -0700

    fix

--- scripts/dev.js
+++ scripts/dev.js
@@ -1,29 +1,28 @@
 // scripts/dev.js
 
-const { exec } = require('child_process');
-const path = require('path');
-const httpServer = require('http-server');
+import { exec } from "child_process";
+import { createServer } from "http-server";
 
 // Start the HTTP server
-const server = httpServer.createServer();
+const server = createServer();
 const port = 8080;
 
 server.listen(port, () => {
-  console.log(`HTTP Server running at http://localhost:${port}`);
+	console.log(`HTTP Server running at http://localhost:${port}`);
 });
 
 // Watch for file changes and restart the server
-const buildCommand = 'cd web && yarn build --watch';
+const buildCommand = "cd web && yarn build --watch";
 const watcher = exec(buildCommand);
 
-watcher.stdout.on('data', (data) => {
-  console.log(`stdout: ${data}`);
+watcher.stdout.on("data", (data) => {
+	console.log(`stdout: ${data}`);
 });
 
-watcher.stderr.on('data', (data) => {
-  console.error(`stderr: ${data}`);
+watcher.stderr.on("data", (data) => {
+	console.error(`stderr: ${data}`);
 });
 
-watcher.on('close', (code) => {
-  console.log(`child process exited with code ${code}`);
+watcher.on("close", (code) => {
+	console.log(`child process exited with code ${code}`);
 });
